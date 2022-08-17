a966fe9fb29be666ac66a66b448da436fa4167d7
azkeever <40367797+azkeever@users.noreply.github.com>
2022-08-17 13:58:17 +0200

    Add files via upload

---
+++ piscine/c04/ex02/ft_putnbr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hpostman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/30 15:32:00 by hpostman          #+#    #+#             */
+/*   Updated: 2016/10/08 16:45:53 by hpostman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Coding of non-standard function putnbr().
+** Parameters: an integer
+** Side effect: prints the number put in to standard out using only write(2) as
+** a standard helper function
+** Return value: none
+*/
+
+#include "libft.h"
+
+void	ft_putnbr(int n)
+{
+	if (n > 2147483647 || n < -2147483648)
+		return ;
+	if (n == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return ;
+	}
+	if (n < 0)
+	{
+		n = n * -1;
+		ft_putchar('-');
+	}
+	if (n < 10)
+	{
+		ft_putchar(n + '0');
+		return ;
+	}
+	ft_putnbr(n / 10);
+	ft_putchar((n % 10) + '0');
+	return ;
+}
